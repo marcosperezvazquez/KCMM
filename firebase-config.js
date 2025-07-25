982a49d5311d5a9c6e9d24bebe6187cc5edd917b
marcosperezvazquez <58280445+marcosperezvazquez@users.noreply.github.com>
2025-07-25 15:59:08 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+
+// IMPORTANT: Replace the following with the configuration object
+// from your own Firebase project's settings.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB24FK40HnQ_EVKZkmBiCvMXU7wFOQ6WOI",
+  authDomain: "gemini-test-aa0b7.firebaseapp.com",
+  projectId: "gemini-test-aa0b7",
+  storageBucket: "gemini-test-aa0b7.firebasestorage.app",
+  messagingSenderId: "410596219691",
+  appId: "1:410596219691:web:d3ec379735ea279fbe436e",
+  measurementId: "G-MDZXM0ZDSF"
+};
